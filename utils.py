61b2e48b418b7131a4aa9a8ccf96c3e717c96be5
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-06-12 12:26:08 +0200

    Add functions to plot learning curves and get features and lables from data

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from sklearn.model_selection import learning_curve
 
 import joblib
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 import re
 import os
 import unicodedata
@@ -102,3 +105,56 @@ def get_config(config_file):
     with open(str(config_file), 'r') as f:
         config = json.loads(f.read())
     return config
+
+
+def plot_learning_curve(estimator, title, X, y, metric, ylim=None, cv=None, 
+                        n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5), 
+                        shuffle=False, save_fig=False):
+    """
+    Generate a simple plot of the test and training learning curve
+    Taken from 
+    https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
+    """
+    
+    plt.figure(figsize=(12,6))
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    
+    train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, 
+        n_jobs=n_jobs, cv=cv, train_sizes=train_sizes, shuffle=shuffle, 
+        scoring=metric)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    
+    plt.grid()
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",label="Cross-validation score")
+
+    plt.legend(loc="best")
+
+    if save_fig:
+        name_fig_file = f'{title} - {metric}.png'
+        if not os.path.exists('figures'):
+            os.mkdir('figures')
+        fig_path = os.path.join('figures', name_fig_file)
+        plt.savefig(fig_path, dpi=200)
+    
+    return plt
+
+
+def get_features_and_labels(file_name):
+    with open(file_name, 'rb') as f:
+        data_dict = joblib.load(f)
+    data = data_dict['data']
+    labels = data['label'].values
+    text_features = list(data.iloc[:,0].values)        
+    extra_features = np.array(data.iloc[:,2:].values)
+    features = np.concatenate((text_features, extra_features), axis=1)
+    return features, labels
