2a6cb54506573a4ea609380b08c058baee183db3
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-06-15 11:52:19 +0200

    Modify parameters of execution

--- train_models.cmd
+++ train_models.cmd
@@ -13,5 +13,5 @@ module load intel
 module load mkl
 module load python/3.7.4
 source env/bin/activate
-python train.py train-models --metric 'balanced_accuracy'
+python train.py train-models --metric 'recall' --num_splits 10
 echo $(date '+%Y-%m-%d %H:%M:%S')
