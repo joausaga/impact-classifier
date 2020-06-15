b6b8c6332690dafda1912913b577b0c150f275b3
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-06-15 09:26:30 +0200

    Application that implements the classifier

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from text_processor import sentence_to_words, count_pos_tag, analyze_sentiment
+
 from nltk.tokenize import sent_tokenize
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from text_processor import sentence_to_words, count_pos_tag, analyze_sentiment, \
+                           clean_sentence, fix_latin_abbreviations, is_valid_sentence
 from utils import get_config
 
-import numpy as np
+import fitz
 import io
 import joblib
+import numpy as np
+import re
 import os
 import pandas as pd
 import pdftotext
@@ -41,19 +45,46 @@ def process_sentence(sentence, vectorizer, vectorizer_type):
     return features
 
 
-def process_pdf(pdf_file, classifier, vectorizer, vectorizer_type):
-    pdf = pdftotext.PDF(pdf_file)
-    num_pages = len(pdf)    
+def process_pdf(pdf_file, file_name, classifier, vectorizer, vectorizer_type):    
+    pdf = fitz.open(stream=pdf_file, filetype='pdf')
+    num_pages = pdf.pageCount
+    impact_sentences = []
+    p_bar = st.progress(0)
     for page_num in range(0, num_pages):
-        st.write(f'Page: {page_num+1}')
-        raw_sentences = sent_tokenize(pdf[page_num])
+        page = pdf.loadPage(page_num)
+        page_text = page.getText("text")
+        page_text = fix_latin_abbreviations(page_text)        
+        page_text = page_text.replace('\n', ' ').replace('\r', ' ')
+        raw_sentences = sent_tokenize(page_text)
         for raw_sentence in raw_sentences:
-            processed_sentence = process_sentence(raw_sentence, vectorizer, 
-                                                  vectorizer_type)            
-            prediction = classifier.predict(processed_sentence)
-            if prediction == 1:
-                st.write(raw_sentences[0])
-
+            clean_text = clean_sentence(raw_sentence)
+            if is_valid_sentence(clean_text):
+                processed_sentence = process_sentence(clean_text, vectorizer, 
+                                                    vectorizer_type)            
+                prediction = classifier.predict(processed_sentence)
+                if prediction[0] == 1:                    
+                    if len(impact_sentences) > 0:
+                        found_same_text = False
+                        for impact_sentence in impact_sentences:
+                            if clean_text == impact_sentence['Sentence']:
+                                found_same_text = True
+                                break
+                    if len(impact_sentences) == 0 or not found_same_text:
+                        impact_sentences.append(
+                            {
+                                'File': file_name if file_name else 'file_name', 
+                                'Page': page_num+1, 
+                                'Sentence': clean_text}
+                        )                    
+            p_bar.progress(round((page_num)/num_pages,0))
+    num_impact_sentences = len(impact_sentences)
+    display_msg = f'Found **{num_impact_sentences}** evidence of social impact in the document of {num_pages} pages' 
+    if num_impact_sentences == 0:
+        st.info(display_msg)
+    else:
+        st.success(display_msg + '. See table below')
+        st.table(impact_sentences)
+    
 
 def init(model_dir, model_name, data_dir):
     # Load model
@@ -97,9 +128,9 @@ def main():
     Impact Classifier is a machine-learning-based document classifier that
     automatically identifies evidence of social impact in research documents.
     """)
-    uploaded_file = st.file_uploader('Select a file to analyze', type='pdf')
+    uploaded_file = st.file_uploader('Select PDF documents to analyze', type='pdf')
     if uploaded_file is not None:
-        process_pdf(uploaded_file, classifier, vectorizer, vectorizer_type)
+        process_pdf(uploaded_file, '', classifier, vectorizer, vectorizer_type)
 
 
 if __name__ == "__main__":
